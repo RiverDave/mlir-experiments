fc0e1c499375f61c0b6a47a1bc2b9aeab5d38f7f
David Rivera <davidriverg@gmail.com>
2025-05-18 16:08:19 -0400

    Add lit testing suite

---
+++ tests/lit.cfg.py
@@ -0,0 +1,29 @@
+import os
+import lit.formats
+
+config.test_format = lit.formats.ShTest(execute_external=True)
+
+# Name of your test suite
+config.name = 'MLIRTests'
+
+
+
+# Test file suffixes
+config.suffixes = ['.mlir']
+#FIXME: This is wrong
+# llvm_project_root_binaries = os.path.abspath(os.path.join(config.test_source_root, '/../externals/llvm-project/build/bin'))
+
+# Substitutions for RUN lines
+config.test_source_root = os.path.dirname(__file__)
+mlir_opt = os.path.abspath(os.path.join(config.test_source_root, '/../mlir-opt'))
+# Not needed for now, Let's use the one in the PATH
+#file_check = os.path.abspath(os.path.join(llvm_project_root_binaries, '/FileCheck'))
+file_check = "FileCheck"
+
+
+
+config.substitutions.append(('%mlir_opt', mlir_opt))
+config.substitutions.append(('%FileCheck', file_check))
+
+# Environment
+config.environment['PATH'] = os.environ['PATH']
