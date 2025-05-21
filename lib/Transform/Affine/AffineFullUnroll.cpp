ffe21d797e7c81ebef68fbc4d9cc4cd29835017a
David Rivera <davidriverg@gmail.com>
2025-05-20 23:40:23 -0400

    Add initial AffineUnrollLoop util pass

--- lib/Transform/Affine/AffineFullUnroll.cpp
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -1,10 +1,23 @@
 #include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/include/mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace tutorial {
 
+    using mlir::affine::AffineForOp;
+    // using mlir::affine::;
+
 void AffineFullUnrollPass::runOnOperation() {
-  return;
+    //affine: loop analysis dialect
+    getOperation().walk([&](affine::AffineForOp op) {
+        if (failed(affine::loopUnrollFull(op))) {
+            op.emitError("Unrolling failed");
+            signalPassFailure();
+        }
+    });
+
 }
 
 } // namespace tutorial
--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -15,15 +15,17 @@ config.suffixes = ['.mlir']
 
 # Substitutions for RUN lines
 config.test_source_root = os.path.dirname(__file__)
-mlir_opt = os.path.abspath(os.path.join(config.test_source_root, '/../mlir-opt'))
+#mlir_opt = os.path.abspath(os.path.join(config.test_source_root, '/../mlir-opt'))
 # Not needed for now, Let's use the one in the PATH
 #file_check = os.path.abspath(os.path.join(llvm_project_root_binaries, '/FileCheck'))
 file_check = "FileCheck"
+tutorial_opt = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tutorial-opt'))
+tutorial_opt_dir = os.path.dirname(tutorial_opt)
 
 
-
-config.substitutions.append(('%mlir_opt', mlir_opt))
+config.substitutions.append(('%mlir_opt', "mlir-opt"))
 config.substitutions.append(('%FileCheck', file_check))
+config.substitutions.append(('%tutorial-opt', tutorial_opt))
 
 # Environment
-config.environment['PATH'] = os.environ['PATH']
+config.environment['PATH'] = tutorial_opt_dir + os.pathsep + os.environ['PATH']
