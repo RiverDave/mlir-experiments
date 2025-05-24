520a1008ce08c6d1dff9649d65c745f830887dc9
David Rivera <davidriverg@gmail.com>
2025-05-24 17:59:47 -0400

    Add Rewrite Pattern AffineUnroll pass

---
+++ make.sh
@@ -0,0 +1,3 @@
+/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/cmake --build /Users/davidfeliperiveraguerra/dev/mlir-intro/build-ninja --target tutorial-opt -j 20
+/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/cmake --build /Users/davidfeliperiveraguerra/dev/mlir-intro/build-ninja --target AffineFullUnroll -j 20
+
